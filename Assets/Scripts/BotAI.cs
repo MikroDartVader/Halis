b85191ba9cb0680d215402e37ecdde63384725ac
MikroDartVader <mike.korovin@gmail.com>
2017-02-19 18:16:23 +0300

    Strange Snake (1.0)
    первый скрипт с летающей змеей на случайном ландшафте

---
+++ Assets/Scripts/BotAI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotAI : MonoBehaviour
+{
+
+	public Transform target;
+	public float moveSpeed;
+	public float rotationSpeed;
+	public float distance;
+
+	private Transform myTrasform;
+
+	void Awake ()
+	{
+		myTrasform = transform;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		/*GameObject go = GameObject.FindGameObjectWithTag ("Player");
+		target = go.transform;*/
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.DrawLine (myTrasform.position, target.position, Color.black);
+
+		//look at target
+		myTrasform.rotation = Quaternion.Slerp (myTrasform.rotation, Quaternion.LookRotation (target.position - myTrasform.position), rotationSpeed * Time.deltaTime);
+
+		//Follow target
+		var heading = target.position - myTrasform.position;
+		myTrasform.position += myTrasform.forward * (heading.magnitude - distance) * Time.deltaTime* moveSpeed; 
+	}
+}
